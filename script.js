893ea86f35c02287379fb191b2de0a9d169cb794
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-05-22 19:33:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', (event) => {
+    const inputField = document.getElementById('fname');
+
+    inputField.addEventListener('blur', () => {
+        inputField.value = inputField.value.toUpperCase();
+    });
+});
+
